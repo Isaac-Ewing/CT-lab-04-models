628f10d94afb4c18964e209bd0dc7479b1f3b0d1
Isaac <isaac.ewing3@gmail.com>
2021-08-02 13:10:16 -0600

    made files and a failing test

---
+++ __tests__/fogs.test.js
@@ -0,0 +1,22 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+
+describe('fog routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('send to fog', async () => {
+    const res = await request(app)
+      .post('/api/v1/fogs')
+      .send({ type: 'radiation fog', formedBy: 'heat transfer' });
+
+    expect(res.body).toEqual({
+      id: '1',
+      type: 'radiation fog',
+      formedBy: 'heat transfer'
+    });
+  });
+});
